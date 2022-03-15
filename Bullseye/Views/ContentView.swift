d7cb4895ad36c329c2e6a9d99c8a468683c457ac
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2022-03-15 13:21:57 -0400

    1. Creating a Model called Game.swift to take care about gameplay logic 2. Show a popup when the user  taps the "Hit Me" button 3. Read the value of the slider after the user taps the "Hit Me" button 4. Organizing the code

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
@@ -16,20 +21,27 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .lineSpacing(4.0)
                 .font(.footnote)
-            Text("89")
+            Text(String(game.target))
                 .kerning(-1.0)
                 .font(.largeTitle)
                 .fontWeight(.black)
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
-            } //: HSTACK
-            Button(action: {}) {
+            }
+            //: HSTACK
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
                 Text("Hit me")
             } //: BUTTON
+            .alert(isPresented: $alertIsVisible, content: {
+                var roundedValue: Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Hello there!"), message: Text("The slider value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+            })
         } //: VSTACK
     }
 }
