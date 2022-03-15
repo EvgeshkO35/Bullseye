dc43ec4b0b6e9376b5e66cb9a44f2c80c1a912cf
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2022-03-14 23:12:59 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Evgenii Lysenko on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
