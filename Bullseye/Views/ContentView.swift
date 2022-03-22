83e7655851a4cf393598c310a0c1339c09f5f814
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2022-03-21 23:31:36 -0400

    1. Create 4 tests for new round, bonus points and restart features 2. Update game logic to add all the features to the game 3. Make sure UI elements in ContentView.swift and BackgroundView.swift also updated and work properly

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -82,7 +82,10 @@ struct HitMeButton: View {
         )
         .alert(isPresented: $alertIsVisible, content: {
             let roundedValue = Int(sliderValue.rounded())
-            return Alert(title: Text("Hello there!"), message: Text("The slider value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+            let points = (game.points(sliderValue: roundedValue))
+            return Alert(title: Text("Hello there!"), message: Text("The slider value is \(roundedValue).\n" + "You scored \(points) points this round."), dismissButton: .default(Text("Awesome!")) {
+                game.startNewRound(points: points)
+            })
         })
     }
 }
